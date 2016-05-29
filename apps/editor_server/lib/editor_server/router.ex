5219297f76fc388e7e0cf3a30219bc7d28e6035d
asolove <asolove@gmail.com>
2016-05-28 21:09:55 -0400

    Browser textarea can execute code inside server.

--- apps/editor_server/lib/editor_server/router.ex
+++ apps/editor_server/lib/editor_server/router.ex
@@ -1,14 +1,30 @@
 defmodule EditorServer.Router do
+  require Logger
+
   use Plug.Router
+  plug Plug.Parsers, parsers: [:urlencoded]
 
   plug :match
   plug :dispatch
 
-  get "/hello" do
-    send_resp(conn, 200, "Hello, world")
+  get "/" do
+    conn 
+    |> put_resp_content_type("text/html")
+    |> send_resp(200, form)
+  end
+
+  post "/send" do
+    output = inspect Code.eval_string(conn.params["code"])
+    conn
+    |> put_resp_content_type("text/html")
+    |> send_resp(200, output)
   end
 
   match _ do
     send_resp(conn, 404, "Not found")
+  end
+
+  def form do
+    "<form action='send' method='post'><textarea name='code'></textarea><input type=submit></form>"
   end
 end
